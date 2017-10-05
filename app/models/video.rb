1a942bba1c3024a41b0cc100e0e5ca0eff1e3bae
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-05 13:14:47 +0900

    [add]  youtube apiを有効化

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,21 @@
+require "google/apis/youtube_v3"
+
 class Video < ApplicationRecord
   belongs_to :room
   after_create_commit { AddVideoBroadcastJob.perform_later self }
 
   def self.add(room_id, video_id)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = Settings.google.api_key
+
+    opt = {
+      id: video_id,
+    }
+    results = service.list_videos([:snippet, :contentDetails], opt)
+    item = results.items[0]
+    snippet = item.snippet
+    title = snippet.title
+
     create! room: Room.find(room_id), video_id: video_id, movie_start_time: Time.now.to_s(:db)
   end
 end
