b6a012242592974255afbcd9bc0c7e2fdbd96dad
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-06 01:57:58 +0900

    [add] 動画開始時間を計算

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,8 @@ class Video < ApplicationRecord
   after_create_commit { AddVideoBroadcastJob.perform_later self }
 
   def self.add(room_id, youtube_video_id)
+    movie_start_time = calc_movie_start_time(room_id)
+
     service = Google::Apis::YoutubeV3::YouTubeService.new
     service.key = Settings.google.api_key
 
@@ -18,9 +20,24 @@ class Video < ApplicationRecord
 
     create! room: Room.find(room_id),
             youtube_video_id: youtube_video_id,
-            movie_start_time: Time.now.to_s(:db),
+            movie_start_time: movie_start_time,
             title: title,
             duration: duration
+  end
+
+  def self.calc_movie_start_time(room_id)
+    last_movie = Video.order(movie_start_time: :desc).find_by(room_id: room_id)
+    now = Time.now
+    if last_movie.blank?
+      movie_start_time = now
+    else
+      last_movie_end_time = last_movie.movie_start_time +
+                            last_movie.duration.sec +
+                            last_movie.duration.min * 60 +
+                            last_movie.duration.hour * 60 * 60
+      movie_start_time = (last_movie_end_time > now) ? last_movie_end_time : now
+    end
+    (movie_start_time + Settings.movie.interval).to_s(:db)
   end
 
   def self.change_duration_format(duration)
@@ -36,6 +53,7 @@ class Video < ApplicationRecord
     items.blank? ? "0" : items[0].delete(target)
   end
 
+  private_class_method :calc_movie_start_time
   private_class_method :change_duration_format
   private_class_method :get_time
 end
