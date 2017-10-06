61c903481986a78514e2262e5f7883f72daf84e2
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-06 12:39:00 +0900

    [change] videoのデータ構造修正

--- db/migrate/20171004144656_create_videos.rb
+++ db/migrate/20171004144656_create_videos.rb
@@ -4,8 +4,8 @@ class CreateVideos < ActiveRecord::Migration[5.1]
       t.references :room, null: false
       t.string :youtube_video_id, null: false
       t.string :title
-      t.time :duration, null: false
-      t.datetime :movie_start_time, null: false
+      t.datetime :video_start_time, null: false
+      t.datetime :video_end_time, null: false
 
       t.timestamps
     end
