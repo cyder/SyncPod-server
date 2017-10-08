de85018a158cc03aa314d51f2a76027dad65a2bb
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-09 00:37:02 +0900

    [add] 接続時にplay_listを返す

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,11 +2,14 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel"
     stream_for current_user
+    room = Room.find(1)
     now_playing_video = Room.find(1).now_playing_video
     if now_playing_video.present?
       RoomChannel.broadcast_to current_user,
                                render_now_playing_video_json(now_playing_video)
     end
+    RoomChannel.broadcast_to current_user,
+                             render_play_list_json(room)
   end
 
   def unsubscribed
@@ -28,6 +31,15 @@ class RoomChannel < ApplicationCable::Channel
                      :youtube_video_id,
                      :title,
                      :current_time
+        end
+      end
+    end
+
+    def render_play_list_json(room)
+      Jbuilder.encode do |json|
+        json.data_type "play_list"
+        json.data do
+          json.play_list room.play_list
         end
       end
     end
