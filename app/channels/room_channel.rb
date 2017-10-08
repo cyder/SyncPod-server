6d4b0016457d930dd1f414ecf7a6c2aa5563d2fc
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-09 00:44:10 +0900

    [change] now_plyaing_video仕様変更
    再生中でない場合も通信する

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -3,11 +3,8 @@ class RoomChannel < ApplicationCable::Channel
     stream_from "room_channel"
     stream_for current_user
     room = Room.find(1)
-    now_playing_video = Room.find(1).now_playing_video
-    if now_playing_video.present?
-      RoomChannel.broadcast_to current_user,
-                               render_now_playing_video_json(now_playing_video)
-    end
+    RoomChannel.broadcast_to current_user,
+                              render_now_playing_video_json(room)
     RoomChannel.broadcast_to current_user,
                              render_play_list_json(room)
   end
@@ -22,15 +19,20 @@ class RoomChannel < ApplicationCable::Channel
 
   private
 
-    def render_now_playing_video_json(video)
+    def render_now_playing_video_json(room)
+      video = room.now_playing_video
       Jbuilder.encode do |json|
         json.data_type "now_playing_video"
         json.data do
-          json.video video,
-                     :id,
-                     :youtube_video_id,
-                     :title,
-                     :current_time
+          if video.present?
+            json.video video,
+                      :id,
+                      :youtube_video_id,
+                      :title,
+                      :current_time
+          else
+            nil
+          end
         end
       end
     end
