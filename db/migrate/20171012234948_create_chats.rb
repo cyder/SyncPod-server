e23da5de60db776d125dddb34612c40c7b19ba27
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-13 08:57:41 +0900

    [add] chat用model作成

---
+++ db/migrate/20171012234948_create_chats.rb
@@ -0,0 +1,10 @@
+class CreateChats < ActiveRecord::Migration[5.1]
+  def change
+    create_table :chats do |t|
+      t.references :room, null: false
+      t.text :message, null: false
+
+      t.timestamps
+    end
+  end
+end
