cb4e1b7b294a1e472d5166f6834ed12a05da2391
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-23 13:14:01 +0900

    [add] User modelにアクセストークンの処理記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :update_access_token!
+
+  def update_access_token!
+    self.access_token = "#{self.id}:#{Devise.friendly_token}"
+    save
+  end
 end
