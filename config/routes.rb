4838b31499107faecc05789f8db1f6a507cce845
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-23 15:31:08 +0900

    [add] sing　up用のapi作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resource :login, only: [:create], controller: :session
+      resource :users, only: [:create]
     end
   end
 
