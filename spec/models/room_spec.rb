5e30b0ace82f142d644db7a7f8289ac266e58024
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-29 00:55:01 +0900

    [clear] テストリファクタリング

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -17,49 +17,54 @@ describe Room do
     it "create video" do
       expect { subject }.to change { Video.count }.by(1)
     end
+    it { expect(subject.youtube_video_id).to eq youtube_video_id }
   end
 
   describe "#calc_video_start_time" do
     subject { room.calc_video_start_time.to_s }
 
-    let(:video) { build(:video, room: room, video_start_time: start_time, video_end_time: end_time) }
+    let(:video) { build(:video, room: room, video_start_time: last_video_start_time, video_end_time: last_video_end_time) }
     let(:room) { build(:room) }
+    let(:expectation) { (start_time + Settings.movie.interval).to_s }
 
     context "when room have not videos" do
-      it { is_expected.to eq((Time.now.utc + Settings.movie.interval).to_s) }
+      let(:start_time) { Time.now.utc }
+      it { is_expected.to eq expectation }
     end
 
     context "when room have a playing video" do
-      let(:start_time) { Time.now.utc - margin }
-      let(:end_time) { Time.now.utc + margin }
+      let(:last_video_start_time) { Time.now.utc - margin }
+      let(:last_video_end_time) { Time.now.utc + margin }
+      let(:start_time) { video.video_end_time }
       before { video.save! }
-      it { is_expected.to eq((video.video_end_time + Settings.movie.interval).to_s) }
+      it { is_expected.to eq expectation }
     end
 
     context "when room do not have a playing video" do
-      let(:start_time) { Time.now.utc - margin }
-      let(:end_time) { Time.now.utc - margin }
+      let(:last_video_start_time) { Time.now.utc - margin }
+      let(:last_video_end_time) { Time.now.utc - margin }
+      let(:start_time) { Time.now.utc }
       before { video.save! }
-      it { is_expected.to eq((Time.now.utc + Settings.movie.interval).to_s) }
+      it { is_expected.to eq expectation }
     end
   end
 
   describe "#now_playing_video" do
     subject { room.now_playing_video }
 
-    let(:video) { build(:video, room: room, video_start_time: start_time, video_end_time: end_time) }
+    let(:video) { build(:video, room: room, video_start_time: last_video_start_time, video_end_time: last_video_end_time) }
     let(:room) { build(:room) }
 
     context "when room have a playing video" do
-      let(:start_time) { Time.now.utc - margin }
-      let(:end_time) { Time.now.utc + margin }
+      let(:last_video_start_time) { Time.now.utc - margin }
+      let(:last_video_end_time) { Time.now.utc + margin }
       before { video.save! }
       it { is_expected.to eq video }
     end
 
     context "when room have not a playing video" do
-      let(:start_time) { Time.now.utc - margin }
-      let(:end_time) { Time.now.utc - margin }
+      let(:last_video_start_time) { Time.now.utc - margin }
+      let(:last_video_end_time) { Time.now.utc - margin }
       before { video.save! }
       it { is_expected.to eq nil }
     end
@@ -68,8 +73,8 @@ describe Room do
   describe "#play_list" do
     subject { room.play_list }
 
-    let(:video1) { build(:video, room: room, video_start_time: start_time1) }
-    let(:video2) { build(:video, room: room, video_start_time: start_time2) }
+    let(:video1) { build(:video, room: room, video_start_time: last_video_start_time1) }
+    let(:video2) { build(:video, room: room, video_start_time: last_video_start_time2) }
     let(:room) { build(:room) }
 
     context "when room do not have a video" do
@@ -77,8 +82,8 @@ describe Room do
     end
 
     context "when room have some videos" do
-      let(:start_time1) { Time.now.utc + margin }
-      let(:start_time2) { Time.now.utc + margin * 2 }
+      let(:last_video_start_time1) { Time.now.utc + margin }
+      let(:last_video_start_time2) { Time.now.utc + margin * 2 }
       before {
         video1.save!
         video2.save!
