a950efca336b46c747d59e010730d17ed405c32f
Shintani Teppei <teppest1215@gmail.com>
2017-11-02 21:29:17 +0900

    [add] duration_to_arrayのテストを書いた

--- spec/models/concerns/video_duration_spec.rb
+++ spec/models/concerns/video_duration_spec.rb
@@ -44,4 +44,23 @@ describe VideoDuration do
       it { is_expected.to eq "5:00:00" }
     end
   end
+
+  describe "#duration_to_array" do
+    subject { duration.send(:duration_to_array, arg) }
+
+    context "when duration = 0:00:05" do
+      let(:arg) { "PT5S" }
+      it { is_expected.to eq [0, 0, 5] }
+    end
+
+    context "when duration = 0:05:00" do
+      let(:arg) { "PT5M0S" }
+      it { is_expected.to eq [0, 5, 0] }
+    end
+
+    context "when duration = 5:00:00" do
+      let(:arg) { "PT5H0M0S" }
+      it { is_expected.to eq [5, 0, 0] }
+    end
+  end
 end
