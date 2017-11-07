f8ab017c2a4a1fce9e91165a5479bf55b838980a
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-11-08 05:36:28 +0900

    [add] 説明文を追加

--- app/models/concerns/youtube.rb
+++ app/models/concerns/youtube.rb
@@ -8,18 +8,20 @@ class Youtube
               :time,
               :thumbnail_url,
               :published,
+              :description,
               :title
 
   def initialize(id)
     service = Google::Apis::YoutubeV3::YouTubeService.new
     service.key = Settings.google.api_key
-    result = service.list_videos("snippet, contentDetails", { id: id }).items[0]
+    result = service.list_videos("snippet, contentDetails, statistics", { id: id }).items[0]
 
     @youtube_video_id = id
     @channel_title = result.snippet.channel_title
     @thumbnail_url = result.snippet.thumbnails.medium.url
     @time = VideoDuration.new(result.content_details.duration)
     @published = result.snippet.published_at
+    @description = result.snippet.description
     @title = result.snippet.title
   end
 end
