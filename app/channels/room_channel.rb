010ea7cff7419bb91e88d203b6fa4a963e90b8d8
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-11-08 15:32:46 +0900

    [clear] 退室時のDB更新を1行にした

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -11,8 +11,7 @@ class RoomChannel < ApplicationCable::Channel
   def unsubscribed
     message = current_user.name + "さんが退室しました。"
     Chat.create! room: @room, chat_type: "logout", message: message
-    log = UserRoomLog.find_by(user: current_user, room: @room, exit_at: nil)
-    log.update(exit_at: Time.now.utc)
+    UserRoomLog.find_by!(user: current_user, room: @room, exit_at: nil).update(exit_at: Time.now.utc)
   end
 
   def now_playing_video
