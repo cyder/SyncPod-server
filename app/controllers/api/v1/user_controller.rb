8bb94ef829f54b042c0907dbc3a3b956a2d6fa0c
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-11-12 17:22:08 +0900

    [add] 数を指定できるようにした。

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::UserController < ApplicationController
   include ApiCommon
 
+  DEFAULT_NUM = 10
+
   def joined_room
-    @rooms = current_user.joined_room
+    num =  params[:num].present? ? params[:num] : DEFAULT_NUM
+    @rooms = current_user.joined_room num
   end
 end
