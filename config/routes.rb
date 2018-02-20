f221bb834c3efe4cfb95987dcff1047c8c6a0106
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-02-20 21:24:59 +0900

    [fix] routesを書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       resource :login, only: [:create], controller: :session
       resource :users, only: [:create]
       resources :rooms, only: [:index, :create, :show]
-      get "joined_rooms", to: "user#joined_rooms"
+      get "joined_rooms", to: "users#joined_rooms"
       get "youtube/search", to: "youtube#search"
       get "youtube/video", to: "youtube#video"
     end
