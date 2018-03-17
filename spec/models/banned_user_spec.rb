0334969c6e95f9cd900ac45046d595fba6f1dd3f
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-03-17 15:45:57 +0900

    [fix] テスト名の修正

--- spec/models/banned_user_spec.rb
+++ spec/models/banned_user_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe UserRoomLog, type: :model do
+describe BannedUser, type: :model do
   it "is valid with correct param" do
     banned_user = build(:banned_user)
     expect(banned_user).to be_valid
