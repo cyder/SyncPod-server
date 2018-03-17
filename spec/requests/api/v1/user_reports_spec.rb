0c13b011a7b19dc86fbd20db32a7c0dbe34721a5
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-03-17 18:37:41 +0900

    [add] user_reportのテストを追加した

---
+++ spec/requests/api/v1/user_reports_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe "user_report" do
+  let(:user) { create(:user) }
+  let(:headers) { { "Authorization" => user.access_token } }
+
+  describe "POST /api/v1/user_report" do
+    let(:message) { "message" }
+    let(:params) { { message: message } }
+
+    context "with invalid params" do
+      let(:message) { nil }
+      it "returns a error" do
+        is_expected.to eq 400
+        body = response.body
+        expect(body).to have_json_path("error")
+      end
+    end
+
+    context "with valid params" do
+      it "returns a success message", :autodoc do
+        is_expected.to eq 200
+        body = response.body
+        expect(body).to be_json_eql(%("success")).at_path("result")
+        expect(body).to be_json_eql(%("#{message}")).at_path("user_report/message")
+      end
+    end
+
+    context "without sign in" do
+      let(:headers) { { "Authorization" => nil } }
+      it "returns a error message" do
+        is_expected.to eq 401
+        expect(body).to have_json_path("error")
+      end
+    end
+  end
+end
