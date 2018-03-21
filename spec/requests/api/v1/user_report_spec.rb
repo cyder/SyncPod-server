17f0ebc5f684a8841518e283f2bca2294243ab93
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-03-21 16:47:09 +0900

    [delete] controllerテストを削除
    requestテストに移行

--- spec/requests/api/v1/user_report_spec.rb
+++ spec/requests/api/v1/user_report_spec.rb
@@ -15,6 +15,7 @@ describe "user_report" do
         body = response.body
         expect(body).to have_json_path("error")
       end
+      it { expect { subject }.to change { UserReport.count }.by(0) }
     end
 
     context "with valid params" do
@@ -24,6 +25,7 @@ describe "user_report" do
         expect(body).to be_json_eql(%("success")).at_path("result")
         expect(body).to be_json_eql(%("#{message}")).at_path("user_report/message")
       end
+      it { expect { subject }.to change { UserReport.count }.by(1) }
     end
 
     context "without sign in" do
@@ -32,6 +34,7 @@ describe "user_report" do
         is_expected.to eq 401
         expect(body).to have_json_path("error")
       end
+      it { expect { subject }.to change { UserReport.count }.by(0) }
     end
   end
 end
