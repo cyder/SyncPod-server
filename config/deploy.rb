05cbf0e1ef13854b6f3ddeb892a4ca5b514035e7
Shintani Teppei <teppest1215@gmail.com>
2018-03-31 17:12:03 +0900

    add readme

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,17 +66,6 @@ namespace :deploy do
     end
   end
 
-  desc "Run seed"
-  task :seed_fu do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, "db:seed_fu"
-        end
-      end
-    end
-  end
-
   after :publishing, :restart
 
   after :restart, :clear_cache do
---
+++ doc/deploy.md
@@ -0,0 +1,10 @@
+# Deploy
+
+## デプロイできるようになるまでの設定
+
+1. localの`~/.ssh/id_rsa`をサーバーに置いてもらう
+
+## 各種コマンド
+
+- deploy: `bundle exec cap production deploy`
+- database create: `bundle exec cap production deploy:db_create`
