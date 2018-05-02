ae7b8f06474aea43d7ffb668e6b33104856cbecb
Mori Atsushi <mori@cyder.tech>
2018-05-02 16:18:02 +0900

    [add] roomにpublicか指定できるようにした

--- spec/requests/api/v1/rooms_spec.rb
+++ spec/requests/api/v1/rooms_spec.rb
@@ -31,7 +31,7 @@ describe "room" do
     end
 
     context "with valid params" do
-      it "returns a room", :autodoc do
+      it "returns a room" do
         is_expected.to eq 200
         body = response.body
         expect(body).to have_json_path("room")
@@ -45,6 +45,17 @@ describe "room" do
       end
 
       it { expect { subject }.to change(Room, :count).by(1) }
+    end
+
+    context "with valid params with public flag" do
+      let(:params) { { room: { name: name, description: description, public: true } } }
+
+      it "returns a room", :autodoc do
+        is_expected.to eq 200
+        body = response.body
+        expect(body).to have_json_path("room")
+        expect(body).to be_json_eql(%("#{room.name}")).at_path("room/name")
+      end
     end
 
     context "without sign in" do
