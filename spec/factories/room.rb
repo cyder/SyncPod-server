1c2966f355c5895bc664f3e5d56cd5f72b847102
Mori Atsushi <mori@cyder.tech>
2018-05-14 09:51:24 +0900

    [add] ルーム作成者を保存するようにしたい

--- spec/factories/room.rb
+++ spec/factories/room.rb
@@ -2,11 +2,13 @@ FactoryBot.define do
   factory :room do
     name "MyString"
     description "MyText"
+    create_user { FactoryBot.create(:user) }
   end
 
   factory :public_room, class: Room do
     name "public room"
     description "MyText"
+    create_user { FactoryBot.create(:user) }
     public true
   end
 end
