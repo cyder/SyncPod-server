41be888971e0289daae60bc1f364170ac3fe89f3
Mori Atsushi <at.sushi.tb@gmail.com>
2019-04-11 22:09:08 +0900

    carrierwaveの設定を変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,17 @@
-if Rails.env.production?
+if Rails.env.production? || Rails.env.staging?
   CarrierWave.configure do |config|
+    config.fog_provider = "fog/aws"
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["S3_SECRET_KEY_ID"],
-      region: "ap-northeast-1",
+      region: ENV["S3_REGION"],
     }
 
     config.fog_directory = ENV["S3_BUCKET_NAME"]
     config.cache_storage = :fog
     config.fog_public = true
+    config.asset_host = ENV["ASSET_HOST"]
   end
 else
   CarrierWave.configure do |config|
